52b8eedbb5e99bb72ad26de114fcf793fdc496fb
Marlon Wang <wbmarlon@163.com>
2018-03-27 19:26:21 +0800

    (0)add some server-start info for greeting. (1)add i18n support.

--- src/main/java/person/marlon/diamond/controller/MyConfig.java
+++ src/main/java/person/marlon/diamond/controller/MyConfig.java
@@ -4,9 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
-import org.springframework.stereotype.Component;
+import person.marlon.diamond.util.CharGraph;
+import person.marlon.diamond.util.I18nUtil;
 
 import javax.annotation.PostConstruct;
+import java.util.Timer;
+import java.util.TimerTask;
 
 @Configuration
 @PropertySource(value = {"classpath:system.properties"},ignoreResourceNotFound=false)
@@ -17,9 +20,19 @@ public class MyConfig {
 
     @PostConstruct
     private void ver(){
-        String greeting = env.getProperty("greeting");
-        System.out.println(greeting);
-        System.out.println("================");
+        System.out.println(env.getProperty("greeting"));
+        System.out.println(CharGraph.generateFoZu());
+        new Timer("dynamic").scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                try{
 
+                    System.out.println(/*new SimpleDateFormat().format()*/I18nUtil.getMessage("author"));
+                }catch(Exception e){
+                    System.out.println(e.getMessage());
+                }
+
+            }
+        },3000,5000);
     }
 }
---
+++ src/main/java/person/marlon/diamond/util/CharGraph.java
@@ -0,0 +1,34 @@
+package person.marlon.diamond.util;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Base64;
+
+public class CharGraph {
+
+    public static String generateFoZu(){
+//        String fozuStrWishPray = "ICAgICAgICAgICAgICAgICAgIF9vb09vb18KICAgICAgICAgICAgICAgICAgbzg4ODg4ODhvCiAgICAgICAg" +
+//                "ICAgICAgICAgIDg4IiAuICI4OAogICAgICAgICAgICAgICAgICAofCAtXy0gfCkKICAgICAgICAgICAgICAgICAgT1wgID0gIC9PCiA" +
+//                "gICAgICAgICAgICAgIF9fX18vYC0tLSdcX19fXwogICAgICAgICAgICAgLicgIFxcfCAgICAgfC8vICBgLgogICAgICAgICAgICAvIC" +
+//                "BcXHx8fCAgOiAgfHx8Ly8gIFwKICAgICAgICAgICAvICBffHx8fHwgLTotIHx8fHx8LSAgXAogICAgICAgICAgIHwgICB8IFxcXCAgL" +
+//                "SAgLy8vIHwgICB8CiAgICAgICAgICAgfCBcX3wgICcnXC0tLS8nJyAgfCAgIHwKICAgICAgICAgICBcICAuLVxfXyAgYC1gICBfX18v" +
+//                "LS4gLwogICAgICAgICBfX19gLiAuJyAgLy0tLi0tXCAgYC4gLiBfXwogICAgICAuIiIgJzwgIGAuX19fXF88fD5fL19fXy4nICA+JyI" +
+//                "iLgogICAgIHwgfCA6ICBgLSBcYC47YFwgXyAvYDsuYC8gLSBgIDogfCB8CiAgICAgXCAgXCBgLS4gICBcXyBfX1wgL19fIF8vICAgLi" +
+//                "1gIC8gIC8KPT09PT09YC0uX19fX2AtLl9fX1xfX19fXy9fX18uLWBfX19fLi0nPT09PT09CiAgICAgICAgICAgICAgICAgICBgPS0tL" +
+//                "T0nCl5eXl5eXl5eXl5eXl5eXl5eXl5eXl5eXl5eXl5eXl5eXl5eXl5eXl5eXl5eXgogICAgICAgICAgICAgICAgIOS9m+elluS/neS9" +
+//                "kSAgICAgICDmsLjml6BCVUc=";
+
+        try {
+            return new String(Base64.getDecoder().decode("ICAgICAgICAgICAgICAgICAgIF9vb09vb18KICAgICAgICAgICAgICAgICA" +
+                    "gbzg4ODg4ODhvCiAgICAgICAgICAgICAgICAgIDg4IiAuICI4OAogICAgICAgICAgICAgICAgICAofCAtXy0gfCkKICAgICAgICAg" +
+                    "ICAgICAgICAgT1wgID0gIC9PCiAgICAgICAgICAgICAgIF9fX18vYC0tLSdcX19fXwogICAgICAgICAgICAgLicgIFxcfCAgICAg" +
+                    "fC8vICBgLgogICAgICAgICAgICAvICBcXHx8fCAgOiAgfHx8Ly8gIFwKICAgICAgICAgICAvICBffHx8fHwgLTotIHx8fHx8LSAgXAog" +
+                    "ICAgICAgICAgIHwgICB8IFxcXCAgLSAgLy8vIHwgICB8CiAgICAgICAgICAgfCBcX3wgICcnXC0tLS8nJyAgfCAgIHwKICAgICAgICAg" +
+                    "ICBcICAuLVxfXyAgYC1gICBfX18vLS4gLwogICAgICAgICBfX19gLiAuJyAgLy0tLi0tXCAgYC4gLiBfXwogICAgICAuIiIgJzwgIGA" +
+                    "uX19fXF88fD5fL19fXy4nICA+JyIiLgogICAgIHwgfCA6ICBgLSBcYC47YFwgXyAvYDsuYC8gLSBgIDogfCB8CiAgICAgXCAgXCBgLS" +
+                    "4gICBcXyBfX1wgL19fIF8vICAgLi1gIC8gIC8KPT09PT09YC0uX19fX2AtLl9fX1xfX19fXy9fX18uLWBfX19fLi0nPT09PT09CiAgI" +
+                    "CAgICAgICAgICAgICAgICBgPS0tLT0nCl5eXl5eXl5eXl5eXl5eXl5eXl5eXl5eXl5eXl5eXl5eXl5eXl5eXl5eXl5eXg=="), "utf-8");
+        } catch (UnsupportedEncodingException e) {
+            return "";
+        }
+    }
+}
